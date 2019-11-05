aa5009f216973a5ae013dea5c6e7e004ec6c56d6
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-11-05 12:24:00 +0100

    Player Movement og basic setup

---
+++ Projekt2-StoryGame-Unity/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 3;
+    public Rigidbody2D RB;
+
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+
+        float inputHori = Input.GetAxis("Horizontal");
+        float inputVerti = Input.GetAxis("Vertical");
+
+        Vector2 movement  = new Vector2(inputHori, inputVerti);
+
+        RB.velocity = movement * speed;
+
+    }
+}
