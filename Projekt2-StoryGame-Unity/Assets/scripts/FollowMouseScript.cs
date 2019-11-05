5668d405209af5db9f09ea5abcc26b13a4f6e249
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-11-05 13:08:17 +0100

    komments

--- Projekt2-StoryGame-Unity/Assets/scripts/FollowMouseScript.cs
+++ Projekt2-StoryGame-Unity/Assets/scripts/FollowMouseScript.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class FollowMouseScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+
+        //finder musens pos i forhold til spilleren
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        //udregner den vinkel spilleren skal have for at kikke mod musen
         var ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
+        //sætter så spilleresn rotation til den udregnede vinkel
         transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+
     }
+
 }
--- Projekt2-StoryGame-Unity/Assets/scripts/PlayerMovement.cs
+++ Projekt2-StoryGame-Unity/Assets/scripts/PlayerMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    //hastigheden spilleren bevægersig
     public float speed = 3;
+
     public Rigidbody2D RB;
 
     void Start()
@@ -16,12 +17,14 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        //modtager input
         float inputHori = Input.GetAxis("Horizontal");
         float inputVerti = Input.GetAxis("Vertical");
 
+        //samler inputsne i en vector2
         Vector2 movement  = new Vector2(inputHori, inputVerti);
 
+        //sætter spillerens fart * hastigheden.
         RB.velocity = movement * speed;
 
     }
