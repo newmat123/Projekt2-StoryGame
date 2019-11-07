4913f0f0014c673a583fb065cb2d1af0e6a40082
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-11-07 14:30:08 +0100

    door triggers

---
+++ Projekt2-StoryGame-Unity/Assets/scripts/cameraMoveScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMoveScript : MonoBehaviour
+{
+
+    public GameObject[] rooms;
+    public GameObject cam;
+
+   
+    private void changeCameraPosTo()
+    {
+
+        if(cam.transform.position == rooms[1].transform.position)
+        {
+            cam.transform.position = rooms[0].transform.position;
+        }
+        else
+        {
+            cam.transform.position = rooms[1].transform.position;
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            changeCameraPosTo();
+        }
+
+    }
+
+}
