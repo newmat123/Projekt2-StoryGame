fac4b7ceb084bf76bd177c940b922461b89815df
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-11-12 11:59:33 +0100

    i

--- Projekt2-StoryGame-Unity/Assets/scripts/cameraMoveScript.cs
+++ Projekt2-StoryGame-Unity/Assets/scripts/cameraMoveScript.cs
@@ -10,11 +10,11 @@ public class cameraMoveScript : MonoBehaviour
 
     public GameObject cam;
 
-    public void ChangeRoomTo(int i)
+    public void ChangeRoomTo(int i, int j)
     {
         //flytter spiller og cam til pos x
         cam.transform.position = camPoses[i].transform.position;
-        this.transform.position = playerStartPoses[i].transform.position;
+        this.transform.position = playerStartPoses[j].transform.position;
 
         //eventuelt trigger dør lyd her
 
@@ -28,23 +28,27 @@ public class cameraMoveScript : MonoBehaviour
         {
             case "Room1":
                 //køre funktionen og siger hvor der skal skiftes til
-                ChangeRoomTo(0);
+                ChangeRoomTo(0, 0);
+                break;
+
+            case "Room11":
+                ChangeRoomTo(0, 3);
                 break;
 
             case "Room2":
-                ChangeRoomTo(1);
+                ChangeRoomTo(1, 1);
                 break;
 
             case "Room3":
-                ChangeRoomTo(2);
+                ChangeRoomTo(2, 2);
                 break;
 
             case "Room4":
-                ChangeRoomTo(3);
+                ChangeRoomTo(3, 3);
                 break;
 
             case "Room5":
-                ChangeRoomTo(4);
+                ChangeRoomTo(4, 4);
                 break;
         }
     }
