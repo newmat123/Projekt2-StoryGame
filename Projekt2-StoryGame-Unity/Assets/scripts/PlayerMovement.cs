0182efa2de9515e0cb3f761f47381a2d116b2604
Agger0897 <38247158+Agger0897@users.noreply.github.com>
2019-11-12 12:26:00 +0100

    Player animation setup

--- Projekt2-StoryGame-Unity/Assets/scripts/PlayerMovement.cs
+++ Projekt2-StoryGame-Unity/Assets/scripts/PlayerMovement.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //hastigheden spilleren bevægersig
-    public float speed = 3;
+    public float speed = 5;
 
-    public Rigidbody2D RB;
+    Rigidbody2D rbody;
+
+    Animator anim;
 
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
     void FixedUpdate()
     {
+
+        
         //modtager input
         float inputHori = Input.GetAxis("Horizontal");
         float inputVerti = Input.GetAxis("Vertical");
@@ -25,7 +30,18 @@ public class PlayerMovement : MonoBehaviour
         Vector2 movement  = new Vector2(inputHori, inputVerti);
 
         //sætter spillerens fart * hastigheden.
-        RB.velocity = movement * speed;
+        rbody.velocity = movement * speed;
 
+        if (movement != Vector2.zero)
+        {
+            anim.SetBool("iswalking", true);
+            anim.SetFloat("X", inputHori);
+            anim.SetFloat("Y", inputVerti);
+        }
+        else
+        {
+            anim.SetBool("iswalking", false);
+        }
+        
     }
 }
