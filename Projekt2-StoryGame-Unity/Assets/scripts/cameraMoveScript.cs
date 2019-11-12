d8990d5b0f597ff25da7dfbc4f65991c4a077688
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-11-12 11:31:22 +0100

    lil update

--- Projekt2-StoryGame-Unity/Assets/scripts/cameraMoveScript.cs
+++ Projekt2-StoryGame-Unity/Assets/scripts/cameraMoveScript.cs
@@ -7,53 +7,44 @@ public class cameraMoveScript : MonoBehaviour
 
     public GameObject[] camPoses;
     public GameObject[] playerStartPoses;
+
     public GameObject cam;
 
-   /*
-    private void changeCameraPosTo()
+    public void ChangeRoomTo(int i)
     {
-        if (cam.transform.position == rooms[1].transform.position)
-        {
-            cam.transform.position = rooms[0].transform.position;
-        }
-        else
-        {
-            cam.transform.position = rooms[1].transform.position;
-        }
+        //flytter spiller og cam til pos x
+        cam.transform.position = camPoses[i].transform.position;
+        this.transform.position = playerStartPoses[i].transform.position;
+
+        //eventuelt trigger dør lyd her
+
     }
-    */
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        /*
-        if(collision.tag == "Player")
-        {
-            changeCameraPosTo();
-        }
-        */
-
+        
         switch (collision.tag)
         {
             case "Room1":
-                cam.transform.position = camPoses[0].transform.position;
-                this.transform.position = playerStartPoses[0].transform.position;
+                //køre funktionen og siger hvor der skal skiftes til
+                ChangeRoomTo(0);
                 break;
 
             case "Room2":
-                cam.transform.position = camPoses[1].transform.position;
-                this.transform.position = playerStartPoses[1].transform.position;
+                ChangeRoomTo(1);
                 break;
 
             case "Room3":
-                cam.transform.position = camPoses[2].transform.position;
+                ChangeRoomTo(2);
                 break;
 
             case "Room4":
-                cam.transform.position = camPoses[3].transform.position;
+                ChangeRoomTo(3);
                 break;
 
             case "Room5":
-                cam.transform.position = camPoses[4].transform.position;
+                ChangeRoomTo(4);
                 break;
         }
     }
