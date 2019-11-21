412bc5cec5aa527037b91fc407a3ba9b52617b10
newmat123 <37107989+newmat123@users.noreply.github.com>
2019-11-21 16:36:48 +0100

    a lot
    pause menu
    bug fix

---
+++ Projekt2-StoryGame-Unity/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    private bool isOpen;
+
+    void Start()
+    {
+        isOpen = false;
+        pauseMenu.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (!isOpen)
+            {
+                open();
+            }
+            else
+            {
+                close();
+            }
+        }
+    }
+
+
+    public void open()
+    {
+        Time.timeScale = 0f;
+        isOpen = true;
+        pauseMenu.SetActive(true);
+    }
+
+    public void close()
+    {
+        Time.timeScale = 1f;
+        isOpen = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
